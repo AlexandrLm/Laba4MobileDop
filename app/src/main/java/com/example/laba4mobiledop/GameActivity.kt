8c68f5642381b818c67d47b641731af2eb16812f
Александр Ламожапов <vikal1709@gmail.com>
2024-03-17 21:54:32 +0800

    pre-done

--- app/src/main/java/com/example/laba4mobiledop/GameActivity.kt
+++ app/src/main/java/com/example/laba4mobiledop/GameActivity.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.material.color.utilities.Score
 
 class GameActivity : AppCompatActivity() {
     private lateinit var chestsContainer: LinearLayout
@@ -22,6 +23,8 @@ class GameActivity : AppCompatActivity() {
     private var score : Int = 0
     private var curRound : Int = 1
     private var curScore : Int = 0
+    private lateinit var settings: Settings
+
 
     private val chestPoints = mutableListOf<Int>() // список очков для каждого сундука
 
@@ -37,8 +40,7 @@ class GameActivity : AppCompatActivity() {
         curRoundText = findViewById(R.id.curRoundText)
         curScoreText = findViewById(R.id.curScoreText)
 
-
-        val settings = intent.getParcelableExtra<Settings>("settings")
+        settings = intent.getParcelableExtra<Settings>("settings")!!
 
         if (settings != null) {
             fillChests(settings.numberOfChests - 5,settings.numberOfTraps)
@@ -51,6 +53,8 @@ class GameActivity : AppCompatActivity() {
     @SuppressLint("ResourceAsColor", "ResourceType")
     private fun fillChests(chests: Int, traps: Int) {
         chests1Container.removeAllViews() // удаляем все существующие кнопки, если есть
+        chestsContainer.removeAllViews()
+
         val few = btn.background
         for (i in 1..5) {
             val chestButton = Button(this)
@@ -92,8 +96,13 @@ class GameActivity : AppCompatActivity() {
             chest.text = "${chestPoints[chest.id - 1]}"
             curScore += chestPoints[chest.id - 1]
         }
-        else
+        else {
             chest.text = "trap"
+            resetRound()
+            if (curRound > settings.numberOfRounds){
+                finish()
+            }
+        }
         updateAllText()
     }
     private fun distributeTrapsAndPoints(chests: Int, traps: Int) {
@@ -124,5 +133,24 @@ class GameActivity : AppCompatActivity() {
         curRoundText.text = getString(R.string.cur_round_text, curRound.toString())
         curScoreText.text = getString(R.string.cur_score_text, curScore.toString())
     }
+    fun resetRound(){
+        curScore = 0
+        curRound++
+        fillChests(settings.numberOfChests - 5,settings.numberOfTraps)
+        distributeTrapsAndPoints(settings.numberOfChests,settings.numberOfTraps) // Распределяем ловушки и очки между сундуками
+        updateAllText()
+    }
 
+    fun newRound(v : View){
+        score += curScore
+        curScore = 0
+        curRound++
+        if (curRound > settings.numberOfRounds){
+            finish()
+            return
+        }
+        fillChests(settings.numberOfChests - 5,settings.numberOfTraps)
+        distributeTrapsAndPoints(settings.numberOfChests,settings.numberOfTraps) // Распределяем ловушки и очки между сундуками
+        updateAllText()
+    }
 }
